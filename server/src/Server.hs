bd3621c07d0873a41271f0a9929d843a218b4f67
kriczq <kriczq@gmail.com>
2018-01-26 15:59:28 +0100

    xd

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -15,30 +15,38 @@ import qualified Network.WebSockets as WS
 type Client = (Text, WS.Connection)
 type ServerState = [Client]
 
+-- | generate empty state of server
 newServerState :: ServerState
 newServerState = []
 
+-- | returns number of connections based on server state
 numClients :: ServerState -> Int
 numClients = length
 
+-- | 
 clientExists :: Client -> ServerState -> Bool
 clientExists client = any ((== fst client) . fst)
 
+-- | adds client to server state
 addClient :: Client -> ServerState -> ServerState
 addClient client clients = client : clients
 
+-- | removes clients from server state
 removeClient :: Client -> ServerState -> ServerState
 removeClient client = filter ((/= fst client) . fst)
 
+-- | send message to all connected clients
 broadcast :: Text -> ServerState -> IO ()
 broadcast message clients = do
     T.putStrLn message
     forM_ clients $ \(_, conn) -> WS.sendTextData conn message
 
+-- | just runs server
 run :: IO ()
 run = do
     state <- newMVar newServerState
     WS.runServer "127.0.0.1" 9160 $ application state
+
 
 application :: MVar ServerState -> WS.ServerApp
 application state pending = do
@@ -72,18 +80,7 @@ application state pending = do
                     let s' = removeClient client s in return (s', s')
                 broadcast (fst client `mappend` " disconnected") s
 
-{-direct :: Text -> Text -> ServerState -> IO ()
-sendDirectMessage user message clients = do
-    let recipient = words (T.unpack msg) !! 1
-    let client = find (\(name, _) -> T.unpack name == recipient) clients
-
-    case client of
-    Nothing -> broadcast
-        ("SYSTEM: " `mappend` user `mappend` " is so stupid. She or he wanna send message to user that does not exist 😳") clients
-    Just (_, conn) ->
-        WS.sendTextData conn (user `mappend` " directly to you: " `mappend` T.pack (drop (8 + length (T.unpack user)) (T.unpack msg)))
--}
-
+-- | 
 receive :: WS.Connection -> MVar ServerState -> Client -> IO ()
 receive conn state (user, _) = forever $ do
     msg <- WS.receiveData conn
@@ -96,7 +93,7 @@ receive conn state (user, _) = forever $ do
             | otherwise -> readMVar state >>= broadcast
                 (user `mappend` ": " `mappend` msg)
 
-
+-- | send direct message to client based on his name
 sendDirectMessage :: Client -> Text -> Text -> ServerState -> IO ()
 sendDirectMessage (user, conn) who msg state = do
     let client = find (\(user, _) -> user == who) state
@@ -109,3 +106,4 @@ sendDirectMessage (user, conn) who msg state = do
                 WS.sendTextData who_conn (user `mappend` " is pinging you")
             else
                 WS.sendTextData who_conn (user `mappend` " to you: " `mappend` msg)
+                
